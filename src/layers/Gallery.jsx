61ef0cefd9a0146918ac2d1a61e11d222a864bbb
Faris Fadhail <farisfadhail@gmail.com>
2023-07-17 20:57:16 +0700

    feat: fix gallery

--- src/layers/Gallery.jsx
+++ src/layers/Gallery.jsx
@@ -8,20 +8,20 @@ export default function Companies() {
 					<h1 className="text-4xl font-medium">Gallery</h1>
 				</div>
 			</div>
-			<div className="grid grid-cols-3 gap-4 mx-12 mb-12">
-				<div className="">
-					<img src={imgGallery} className="mb-4" />
-					<img src={imgGallery} />
+			<div className="grid grid-cols-12 gap-4 mx-12 mb-12">
+				<div className="col-span-4 row-span-1">
+					<img src={imgGallery} className="mb-4 rounded-lg" />
+					<img src={imgGallery} className="rounded-lg" />
 				</div>
-				<div>
-					<div className="grid-cols-2 grid h-1/2 gap-4 mb-2">
-						<img src={imgGallery} className="max-w-full object-cover h-full" />
-						<img src={imgGallery} className="max-w-full object-cover h-full" />
+				<div className="col-span-5 row-span-1">
+					<div className="grid-cols-2 grid gap-4 h-[48.7%] mb-4">
+						<img src={imgGallery} className="rounded-lg object-cover h-full" />
+						<img src={imgGallery} className="rounded-lg object-cover h-full" />
 					</div>
-					<img src={imgGallery} />
+					<img src={imgGallery} className="object-cover rounded-lg h-[48.3%] w-full" />
 				</div>
-				<div className="row-span-1">
-					<img src={imgGallery} className="object-cover h-full" />
+				<div className="row-span-1 col-span-3">
+					<img src={imgGallery} className="object-cover h-full rounded-lg" />
 				</div>
 			</div>
 		</>
