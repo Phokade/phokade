018375aae2172b0f4e44a9ece747880f585c36c7
ChristYoga123 <christianuswibisono@gmail.com>
2023-07-21 13:59:36 +0700

    feat: finist pricing mobile

--- src/layers/Pricing.jsx
+++ src/layers/Pricing.jsx
@@ -46,9 +46,9 @@ function Pricing() {
 	return (
 		<>
 			<div name="pricing" className="w-full h-screen flex justify-center items-center font-roboto text-center">
-				<div className="flex flex-col">
-					<h1 className="font-medium text-4xl">Paket Jasa Layanan</h1>
-					<p className="mt-10 text-xl">Paket jasa untuk memudahkan proses bisnis Anda.</p>
+				<div className="flex flex-col leading-3">
+					<h1 className="font-medium text-3xl md:text-4xl">Paket Jasa Layanan</h1>
+					<p className="mt-10 px-5 md:text-xl">Paket jasa untuk memudahkan proses bisnis Anda.</p>
 					<div className="flex gap-20 mt-16">
 						{pricings.map((pricing) => (
 							<PricingCard key={pricing.id} image={pricing.image} title={pricing.title} description={pricing.description} features={pricing.benefits} />
