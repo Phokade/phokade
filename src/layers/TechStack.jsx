1491553dbc6411746ad132166c659314402ca6b9
Phokade <141470954+Phokade@users.noreply.github.com>
2023-09-13 03:03:30 +0700

    Update TechStack.jsx

--- src/layers/TechStack.jsx
+++ src/layers/TechStack.jsx
@@ -6,8 +6,8 @@ export default function TechStack() {
 		<>
 			<div className="w-full h-[600px] flex justify-center items-center font-roboto">
 				<div className="flex flex-col font-roboto text-center gap-8 md:gap-12 px-5">
-					<h1 className="text-3xl md:text-4xl font-medium">Teknologi yang Digunakan</h1>
-					<p className="md:text-2xl leading-none text-sm">Beberapa teknologi untuk membantu kebutuhan bisnis Anda!</p>
+					<h1 className="text-3xl md:text-4xl font-medium">ALAT PENDUKUNG INSTALASI</h1>
+					<p className="md:text-2xl leading-none text-sm">Menyediakan Alat Pendukung Penggilingan Padi!</p>
 					<div className="grid md:grid-cols-4 grid-cols-2 gap-4">
 						{[1, 2, 3, 4].map((i) => (
 							<img key={i} src={imgGallery} alt="" />
