34302c8926a0051a1964cfc1375275609fc7e8ee
Phokade <141470954+Phokade@users.noreply.github.com>
2023-09-13 02:57:24 +0700

    Update Pricing.jsx

--- src/layers/Pricing.jsx
+++ src/layers/Pricing.jsx
@@ -58,7 +58,7 @@ export default function Pricing() {
 			<div name="pricing" className="w-full md:mt-24 md:mb-8 mb-32 flex flex-col md:justify-center items-center font-roboto text-center">
 				<div className="flex flex-col justify-center">
 					<h1 className="font-medium text-3xl md:text-4xl">Paket Jasa Layanan</h1>
-					<p className="mt-10 px-5 md:text-xl">Paket jasa untuk memudahkan proses bisnis Anda.</p>
+					<p className="mt-10 px-5 md:text-xl">Jasa Perencanaan Pabrik Penggilingan Padi .</p>
 					<div className="flex md:flex-row flex-col gap-4 md:gap-20 mt-8 md:mt-16 m-auto">
 						{pricings.map((pricing, id) => (
 							<PricingCard key={id} image={pricing.image} title={pricing.title} description={pricing.description} features={pricing.benefits} />
